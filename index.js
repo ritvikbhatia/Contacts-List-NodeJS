be2253ef85248d1c01ae02c8512885c23da6a700
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-23 18:17:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const path=require('path');
+const port=8000;
+const app=express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+var pussy=[{"name":"honey","phone":9},{"name":"yoyo","phone":98}]
+app.get('/',function(req,res){
+    res.render('home',{Pussy:pussy});
+});
+app.post('/yo',function(req,res)
+{
+    console.log(req.body);
+    pussy.push(req.body);
+    res.redirect('/');
+})
+app.listen(port,function(err){
+    if(err)
+    console.log(err);
+})
