2ee8cb6b58934a027c0bc2e862ebdf59e07e8779
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-05-24 22:02:31 +0530

    permanent storage

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ const express=require('express');
 const path=require('path');
 const port=8000;
 const db=require("./config/mongoose");
+const Contact=require('./models/contact')
 const app=express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded());
 app.use(express.static('assets'));
-var pussy=[{"name":"honey","phone":9},{"name":"yoyo","phone":98}]
 app.get('/',function(req,res){
-    res.render('home',{Pussy:pussy});
+    Contact.find({},function(err,data)
+    {
+        if(err)
+        console.log(err);
+        res.render('home',{Pussy:data});
+
+    })
+    
 });
 app.post('/yo',function(req,res)
 {
-    pussy.push(req.body);
+    Contact.create({
+        "name":req.body.name,
+        "phone":req.body.phone
+    },function(err)
+    {
+        if(err)
+        console.log(err);
+        console.log("*****success");
+    })
     res.redirect('/');
 })
 app.post('/clear',function(req,res)
